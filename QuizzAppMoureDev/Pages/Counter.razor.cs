5e25ca78fa517a17703cf5f9d3c4548c07775b97
eecuriel <eduardocuc@hotmail.com>
2022-03-11 17:08:43 -0500

    Add project files.

---
+++ QuizzAppMoureDev/Pages/Counter.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace QuizzAppMoureDev.Pages
+{
+    public class CounterBase  : ComponentBase
+    {
+        public int currentCount { get; set; }
+        
+
+        public  void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
