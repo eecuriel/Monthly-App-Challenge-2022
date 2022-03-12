fb0e1e5580a49bc914bb662890e01ea9038e2bdc
eecuriel <eduardocuc@hotmail.com>
2022-03-12 16:56:39 -0500

    UI model and transition

--- QuizzAppMoureDev/Program.cs
+++ QuizzAppMoureDev/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using QuizzAppMoureDev;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +10,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
+
 
 await builder.Build().RunAsync();
